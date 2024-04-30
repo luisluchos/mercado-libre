b12b8ca301316e13afe46a5c27988e55ac9a7440
Luis <lmarin@aiudo.es>
2024-04-30 16:16:23 +0200

    logs

--- app/[[...segments]]/page.tsx
+++ app/[[...segments]]/page.tsx
@@ -4,8 +4,6 @@ import { getProducts } from "../services";
 export default async function ProductsPage({ params }: { params: { segments: string[] } }) {
 
 const [segments] = params.segments  // nos devuelve un array y solo queremos el primer elemento
-console.log(segments)
   const products = await getProducts(segments);
-  console.log(products)
   return <Products products={products} />;
 }
